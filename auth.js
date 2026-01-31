5a58b7f573e185e0215df1b908cfd575d0143c57
shreevarisnacks-cloud <shreevarisnacks@gmail.com>
2026-01-31 23:13:55 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPVRH8VPpDhGVMF6NZFs7SXFUDVADpmg8",
+  apiKey: "AIzaSyAPVRH8VPPdhGVMF6NZFs7SXFUDVADpmg8",
   authDomain: "shreevarisnacks-45d0d.firebaseapp.com",
   projectId: "shreevarisnacks-45d0d",
   storageBucket: "shreevarisnacks-45d0d.firebasestorage.app",
